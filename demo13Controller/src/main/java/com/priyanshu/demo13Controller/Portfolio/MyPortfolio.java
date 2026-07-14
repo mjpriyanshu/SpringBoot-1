5729c2bec0d91098c2355701d9335c591e3bdcd4
Priyanshu <hkm780414@gmail.com>
2026-07-14 11:14:34 +0530

    Portfolio: Skills added

--- demo13Controller/src/main/java/com/priyanshu/demo13Controller/Portfolio/MyPortfolio.java
+++ demo13Controller/src/main/java/com/priyanshu/demo13Controller/Portfolio/MyPortfolio.java
@@ -12,7 +12,7 @@ public class MyPortfolio {
         return """
                 <div>
                 <H1 style="color: orange;"> Myself </H1>
-                <p style="color: gray;"> My Name is Priyanshu. I am Fourth Year Btech Student. </p>
+                <p style="color: gray;"> My Name is Priyanshu. I am Final Year Btech Student. </p>
                 <ul>
                     <li>Github Link: <a href="https://www.github.com/mjpriyanshu" target="_blank">https://www.github.com/mjpriyanshu</a></li>
                     <li>LeetCode Link: <a href="https://www.leetcode.com/mjpriyanshu" target="_blank">https://www.leetcode.com/mjpriyanshu</a></li>
@@ -20,6 +20,24 @@ public class MyPortfolio {
                 </div>
                 """;
     }
+
+    @GetMapping("/skills")
+    public String Skills() {
+        return """
+                <div>
+                <H1 style="color: orange;"> Skills </H1>
+                <ul>
+                    <li>Java</li>
+                    <li>Springboot</li>
+                    <li>React JS</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+                </div>
+                """;
+    }
+
 
 
 
