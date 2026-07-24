d9abb2cf311e7c2acb6f0071ab006a65f569335b
Priyanshu <hkm780414@gmail.com>
2026-07-24 12:19:03 +0530

    Basic React vite template with About component

---
+++ React/react-pep/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import About from './Components/About'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>Hello world</div>
+      <br></br>
+      <About />
+    </>
+  )
+}
+
+export default App
---
+++ React/react-pep/src/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <div>Hey, This is Priyanshu</div>
+        <p>We are currently learning basic of JSX in react</p>
+    </>
+    
+  )
+}
+
+export default About
